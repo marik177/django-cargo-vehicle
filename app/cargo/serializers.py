65ff41faa87140659ed1731f3ce54f3f22e76de4
Roman K <marik177@gmail.com>
2024-03-26 16:21:07 +0200

    CargoSerializer delivery on zip

--- app/cargo/serializers.py
+++ app/cargo/serializers.py
@@ -5,13 +5,22 @@ from .models import Location, Cargo, Vehicle
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
-        fields = "__all__"
+        fields = ["zip_code"]
 
 
 class CargoSerializer(serializers.ModelSerializer):
+    delivery = LocationSerializer()
+
     class Meta:
         model = Cargo
-        fields = "__all__"
+        fields = ["id", "pick_up", "delivery", "weight", "description"]
+
+    def create(self, validated_data):
+        zip_code = validated_data.pop("delivery").get("zip_code")
+        delivery_location = Location.objects.get(zip_code=zip_code)
+        validated_data["delivery"] = delivery_location
+        print(validated_data)
+        return "qwerty"
 
 
 class VehicleSerializer(serializers.ModelSerializer):
