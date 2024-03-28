956f598061d987a42b61f7e4a0fbe057e0acd3bc
Roman K <marik177@gmail.com>
2024-03-28 09:31:14 +0200

    add swagger drf-yasg

--- app/drf_project/urls.py
+++ app/drf_project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .yasg import urlpatterns as doc_url
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("cargo.urls")),
 ]
+
+urlpatterns += doc_url
